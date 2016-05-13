1b7324674167e29c57fb151ed3914fd1d9725d3f
Tyler Cross <tcross@ansible.com>
2016-05-13 12:33:40 -0400

    Use tower-cli instead of curl

--- deploy-app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -f -k -H 'Content-Type: application/json' -XPOST \
-  -d '{"extra_vars": "{\"commit\": \"'"${TRAVIS_COMMIT}"'\"}"}' \
-  --user ${TOWER_USER}:${TOWER_PASSWORD} \
-  http://${TOWER_HOST}/api/v1/job_templates/8216/launch/
